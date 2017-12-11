3454f132579596b6c7c2d05a6d6f78e323752417
mayadennise2001 <maya_ramirez@davincischools.org>
2017-12-11 13:06:18 -0800

    Add files via upload

--- SuperFanProject/makeup.js
+++ SuperFanProject/makeup.js
@@ -1,3 +1,35 @@
 function m5(elem){
+	document.getElementById("main").classList.remove("destiny");
+	document.getElementById("main").classList.remove("lane2");
+	document.getElementById("main").classList.remove("lane");
+	document.getElementById("main").classList.remove("lightbrown");
 	document.getElementById("main").classList.toggle("peach");
+}
+function m2(elem){
+	document.getElementById("main").classList.remove("peach");
+	document.getElementById("main").classList.remove("lightbrown");
+	document.getElementById("main").classList.remove("lane");
+	document.getElementById("main").classList.remove("lane2");
+	document.getElementById("main").classList.toggle("destiny");
+}
+function m3(elem){
+	document.getElementById("main").classList.remove("destiny");
+	document.getElementById("main").classList.remove("lane2");
+	document.getElementById("main").classList.remove("lane");
+	document.getElementById("main").classList.remove("peach");
+	document.getElementById("main").classList.toggle("lightbrown");
+}
+function m(elem){
+	document.getElementById("main").classList.remove("destiny");
+	document.getElementById("main").classList.remove("lane2");
+	document.getElementById("main").classList.remove("peach");
+	document.getElementById("main").classList.remove("lightbrown");
+	document.getElementById("main").classList.toggle("lane");
+}
+function m6(elem){
+	document.getElementById("main").classList.remove("destiny");
+	document.getElementById("main").classList.remove("peach");
+	document.getElementById("main").classList.remove("lightbrown");
+	document.getElementById("main").classList.remove("lane");
+	document.getElementById("main").classList.toggle("lane2");
 }
