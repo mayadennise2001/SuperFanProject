1a86178522052bdc1609ef4731701dfd1fa90468
mayadennise2001 <maya_ramirez@davincischools.org>
2017-12-12 16:24:38 -0800

    Add files via upload

--- SuperFanProject/makeup.js
+++ SuperFanProject/makeup.js
@@ -1,4 +1,6 @@
 function m5(elem){
+	document.getElementById("main").classList.remove("domo3");
+	document.getElementById("main").classList.remove("des5");
 	document.getElementById("main").classList.remove("destiny");
 	document.getElementById("main").classList.remove("lane2");
 	document.getElementById("main").classList.remove("lane");
@@ -6,6 +8,8 @@ function m5(elem){
 	document.getElementById("main").classList.toggle("peach");
 }
 function m2(elem){
+	document.getElementById("main").classList.remove("domo3");
+	document.getElementById("main").classList.remove("des5");
 	document.getElementById("main").classList.remove("peach");
 	document.getElementById("main").classList.remove("lightbrown");
 	document.getElementById("main").classList.remove("lane");
@@ -13,6 +17,8 @@ function m2(elem){
 	document.getElementById("main").classList.toggle("destiny");
 }
 function m3(elem){
+	document.getElementById("main").classList.remove("domo3");
+	document.getElementById("main").classList.remove("des5");
 	document.getElementById("main").classList.remove("destiny");
 	document.getElementById("main").classList.remove("lane2");
 	document.getElementById("main").classList.remove("lane");
@@ -20,6 +26,8 @@ function m3(elem){
 	document.getElementById("main").classList.toggle("lightbrown");
 }
 function m(elem){
+	document.getElementById("main").classList.remove("domo3");
+	document.getElementById("main").classList.remove("des5");
 	document.getElementById("main").classList.remove("destiny");
 	document.getElementById("main").classList.remove("lane2");
 	document.getElementById("main").classList.remove("peach");
@@ -27,9 +35,29 @@ function m(elem){
 	document.getElementById("main").classList.toggle("lane");
 }
 function m6(elem){
+	document.getElementById("main").classList.remove("domo3");
+	document.getElementById("main").classList.remove("des5");
 	document.getElementById("main").classList.remove("destiny");
 	document.getElementById("main").classList.remove("peach");
 	document.getElementById("main").classList.remove("lightbrown");
 	document.getElementById("main").classList.remove("lane");
 	document.getElementById("main").classList.toggle("lane2");
+}
+function m7(elem){
+	document.getElementById("main").classList.remove("domo3");
+	document.getElementById("main").classList.remove("lane2");
+	document.getElementById("main").classList.remove("destiny");
+	document.getElementById("main").classList.remove("peach");
+	document.getElementById("main").classList.remove("lightbrown");
+	document.getElementById("main").classList.remove("lane");
+	document.getElementById("main").classList.toggle("des5");
+}
+function m8(elem){
+	document.getElementById("main").classList.remove("des5");
+	document.getElementById("main").classList.remove("lane2");
+	document.getElementById("main").classList.remove("destiny");
+	document.getElementById("main").classList.remove("peach");
+	document.getElementById("main").classList.remove("lightbrown");
+	document.getElementById("main").classList.remove("lane");
+	document.getElementById("main").classList.toggle("domo3");
 }
